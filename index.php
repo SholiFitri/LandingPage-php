c8753df22800ed7080b8fdeddb4c12a4d167433e
SholiFitri <151607563+SholiFitri@users.noreply.github.com>
2024-01-13 14:16:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Landing Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<img src="nuggets.jpg" alt=" nuggets background" width="100%" height="100%">
+	</div>
+	<div class="wrapper">
+		<div class="nav">
+			<div class="logo">
+				<a href="#">
+					<p>Chicken Nuggets</p>
+				</a>
+			</div>
+			<ul>
+				<li>Profile</li>
+				<li>Contact</li>
+				<li>Pricing</li>
+			</ul>
+		</div>
+		<div class="header">
+			<h1>Chicken<span>Nuggets</span></h1>
+		</div>
+		<div class="content-header">
+			<p>Chicken nuggets sangat populer di seluruh dunia dan sering dijadikan pilihan favorit, terutama di kalangan anak-anak dan remaja </p>
+			<br>
+			<input type="text" class="search-input" placeholder="Email Address">
+			<input class="search-btn"type= "button" value="Sign Up">
+		</div>
+	</div>
+</body>
+</html>
